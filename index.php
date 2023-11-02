4b803f1c1015bdee25e503e747c2647a6f0e89e7
srushtiphutane26 <68937681+srushtiphutane26@users.noreply.github.com>
2023-11-02 20:48:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Burger Heaven</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/mediaquerry.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+<body>
+    
+    <!-- navbar start here  -->
+    <nav class="bh-navbar">
+        <div class="wrapper">
+            <div class="logo bh-logo"><a href="#" data-aos="fade-up">Burger Heaven</a></div>
+            <input type="radio" name="slider" id="menu-btn">
+            <input type="radio" name="slider" id="close-btn">
+            <ul class="nav-links">
+            <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+            <li class="bh-li-nav-links"><a href="#">About</a></li>
+            <li class="bh-li-nav-links"><a href="#">Menu</a></li>
+            <button type="submit" class="bh-order-btn">Order Online</button>
+            </ul>
+            <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+        </div>
+    </nav>
+    <!-- navbar end here  -->
+
+    <!-- banner section start -->
+    <div class="main-section-bh">
+        <div class="container-fluid main-banner-bh">
+            <p class="main-banner-p">NEW YORK’S <br>FAVORITE ORGANIC<br> HAMBURGER JOINT</p>
+            <img src="assets/images/Property-1-Default.png" class="bh-property-1-default" alt="">
+        </div>
+    </div>
+    <!-- banner section end -->
+
+
+    <!-- about burger start -->
+    <div class="about-section-bh">
+        <div class="container about-bh-cont">
+            <div class="row about-bh-r">
+                <div class="col-lg-6 a-bh-col-1">
+                    <p class="about-bh-p-6">the burger above all burgers</p>
+                </div>
+                <div class="col-lg-6 a-bh-col-1">
+                    <p class="about-bh-p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ornare neque.</p>
+                </div>
+            </div>
+        </div>
+        <img src="assets/images/Property-1-Default.png" class="bh-property-2-default" alt="">
+    </div>
+    <!-- about burger end  -->
+
+    <!-- menu section start  -->
+    <div class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <img src="assets/images/Rectangle 17.png" class="menu-fries-bh" alt="">
+                        <div class="container svff-menu">
+                            <h4 class="svff-h4"><b>salt & Vinegar<br> french fries</b></h4> 
+                            <p class="svff-p">order online</p> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <img src="assets/images/Rectangle 16.png" class="menu-fries-bh" alt="">
+                        <div class="container svff-menu">
+                            <h4 class="svff-h4"><b>crispy chicken <br> sandwich</b></h4> 
+                            <p class="svff-p">order online</p> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- menu section end  -->
+
+    <div class="cta-text">
+        <h2 class="bh-cta-text">he best burger i’ve ever had</h2>
+    </div>
+
+    <div class="customer-testimonal">
+        <img src="assets/images/Rectangle 21.png" class="ct-rec-21" alt="">
+    </div>
+
+    <div class="customer-testimonal-side">
+        <img src="assets/images/Vector.png" class="vec-left-21" alt="">
+    </div>
+
+    <!-- cta section  -->
+    <div class="bh-cta">
+        <h2 class="smm-service-h2">order online or<br> come visit us today</h2>
+        <button type="submit" class="bh-cta-btn">Get Started</button>
+    </div>
+
+
+    <div class="customer-testimonal-right">
+        <img src="assets/images/Vector-1.png" class="vec-left-22" alt="">
+    </div>
+
+    <footer class="bh--footer">
+        <div class="mf-sub-sec">
+            <div class="container">
+                <div class="main--footer-sec">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="footer__logo">
+                                <a href="#" class="bh-logo-f">Burger<br> Heaven</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <h2 class="footer__widget--title">More</h2>
+                            <ul class="footer__widget--menu footer__widget--inner">
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="contact.html">About</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="about.html">Menu</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="wishlist.html">Locations</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="privacy-policy.html">Privacy</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3">
+                        <h2 class="footer__widget--title">Hours</h2>
+                            <ul class="footer__widget--menu footer__widget--inner">
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="contact.html">Open from</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="about.html">11 AM to</a></li>
+                                <li class="footer__widget--menu__list"><a class="footer__widget--menu__text" href="wishlist.html">11 PM daily</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3">
+                            <h2 class="footer__widget--title ">Join Our Newsletter</h2>
+                            <div class="newsletter__subscribe footer__newsletter">
+                                <form class="newsletter__subscribe--form" action="#">
+                                    <label>
+                                        <input class="newsletter__subscribe--input" placeholder=" Enter Your Email" type="text">
+                                    </label>
+                                    <button class="newsletter__subscribe--button" type="submit">Submit</button>
+                                </form>   
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__bottom">
+                <div class="container">
+                    <div class="footer__bottom--inenr d-flex justify-content-between align-items-center">
+                        <p class="copyright__content mb-0">Copyright Burger Heaven</p>
+                        <div class="footer__payment">
+                            <p class="copyright__content mb-0">Privacy Policy</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script  type="text/javascript">
+        AOS.init();
+    </script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
