c68dce096c655152f2c7f32c1deff299ca0ed0d8
srushtiphutane26 <68937681+srushtiphutane26@users.noreply.github.com>
2023-11-02 21:05:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Burger1 Heaven</title>
+    <title>Burger Heaven</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/mediaquerry.css">
     <!-- Bootstrap CSS -->
